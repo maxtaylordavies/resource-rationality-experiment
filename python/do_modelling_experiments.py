f081e8d0c3b928753c167a724d67ed30434d18a1
Max Taylor-Davies <maxtaylordavies@gmail.com>
2024-01-31 13:24:23 +0000

    updates computational experiments

--- python/do_modelling_experiments.py
+++ python/do_modelling_experiments.py
@@ -1,101 +1,180 @@
+from itertools import product
 import time
 
 import jax.numpy as jnp
 from jax import random
 from jax.config import config
+from typing import List
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from tqdm import tqdm
 
-from src.utils import avg_pool_1d
+from src.utils import (
+    avg_pool_1d,
+    aggregated_covariance_matrix,
+    gaussian_entropy,
+    save_figure,
+)
 from src.modelling import (
-    expected_return_optimal,
-    expected_return_approx,
+    expected_acc_approx,
+    sample_covariance_matrix,
     sample_utility_function,
     simulate_choices,
     simulate_predictions,
-    choose,
 )
 
-
+pd.options.mode.chained_assignment = None
 config.update("jax_enable_x64", True)
 sns.set_theme()
 
 
-def state_agg_experiment(n_seeds: int, betas, n_options=32):
+def state_agg_experiment(n_seeds: int, betas: List[float], n_options=128):
     results = []
-    pool_sizes = [2**i for i in range(int(jnp.log2(n_options)) + 1)]
+    pool_sizes = [int(2**i) for i in range(int(jnp.log2(n_options)) + 1)]
+    cov = sample_covariance_matrix(n_options, lengthscale=1)
 
-    for seed in tqdm(range(n_seeds)):
-        for beta in betas:
-            rng_key = random.PRNGKey(seed)
-            u = sample_utility_function(
-                rng_key, n_options, lengthscale=2, min_val=0, max_val=1
-            )
-            pairs, choices = simulate_choices(rng_key, u, beta, n_trials=1000)
-
-            for ps in pool_sizes:
-                u_hat = avg_pool_1d(u, ps)
+    for _ in tqdm(range(n_seeds)):
+        seed = int(time.time())
+        rng_key = random.PRNGKey(seed)
+        u = sample_utility_function(rng_key, cov, min_val=-1, max_val=1)
+        for ps in pool_sizes:
+            u_hat = avg_pool_1d(u, ps)
+            cost = gaussian_entropy(aggregated_covariance_matrix(cov, ps))
+            for beta in betas:
+                # pairs, choices = simulate_choices(rng_key, u, beta, n_trials=1000)
                 results.append(
                     {
                         "beta": beta,
                         "pool_size": ps,
-                        "accuracy": expected_return_approx(u, u_hat, beta),
+                        "accuracy": expected_acc_approx(u, u_hat, beta),
+                        "cost": float(cost),
                         "type": "predicted",
+                        "seed": seed,
                     }
                 )
-                results.append(
-                    {
-                        "beta": beta,
-                        "pool_size": ps,
-                        "accuracy": simulate_predictions(pairs, choices, u_hat),
-                        "type": "empirical",
-                    }
-                )
+                # results.append(
+                #     {
+                #         "beta": beta,
+                #         "pool_size": ps,
+                #         "accuracy": simulate_predictions(pairs, choices, u_hat),
+                #         "cost": float(cost),
+                #         "type": "empirical",
+                #         "seed": seed,
+                #     }
+                # )
 
-    return pd.DataFrame(results)
+    results = pd.DataFrame(results)
+    results["cost"] = results["cost"] / results["cost"].max()
+    results["cost"][results["cost"] < 0.0] = 0.0
+    return results
 
 
-num_seeds = 5
-betas = [0.01, 0.2, 0.5, 1.0]
+num_seeds = 100
+lambdas = [0.1, 0.5, 1.0, 5.0]
+betas = [0.01, 0.1, 1.0, 5.0]
 results = state_agg_experiment(num_seeds, betas, n_options=512)
 
-fig, ax = plt.subplots(figsize=(8, 4))
+# accuracy and efficient frontier plots
+palette = sns.color_palette("husl", n_colors=len(betas))
+# fig, axs = plt.subplots(1, 2, figsize=(12, 4), sharey=True)
+# sns.lineplot(
+#     data=results,
+#     x="pool_size",
+#     y="accuracy",
+#     hue="beta",
+#     style="type",
+#     markers=True,
+#     dashes=True,
+#     ax=axs[0],
+#     palette=palette,
+# )
+# axs[0].set_xscale("log", base=2)
+# axs[0].set(
+#     title="Theoretical vs empirical accuracy using state-aggregated representations",
+#     xlabel="Patch size",
+#     ylabel="Choice prediction accuracy",
+#     ylim=(0.45, 1.05),
+# )
+# handles, labels = axs[0].get_legend_handles_labels()
+# labels[0] = "$\\beta$"
+# axs[0].legend(handles=handles, labels=labels)
+
+results = results[results["type"] == "predicted"]
+# sns.lineplot(
+#     data=results,
+#     x="cost",
+#     y="accuracy",
+#     hue="beta",
+#     ax=axs[1],
+#     palette=palette,
+#     legend=False,
+# )
+# axs[1].set(
+#     title="Accuracy-cost efficient frontier for state-aggregated representations",
+#     xlabel="Cost",
+# )
+# save_figure(fig, "state_agg_accuracy")
+
+# # cost-adjusted value plot
+# fig, axs = plt.subplots(1, len(lambdas), sharex=True, figsize=(16, 4))
+# for i, l in enumerate(lambdas):
+#     results["value"] = results["accuracy"] - 0.5 - (l * results["cost"])
+#     sns.lineplot(
+#         data=results,
+#         x="pool_size",
+#         y="value",
+#         hue="beta",
+#         markers=True,
+#         ax=axs[i],
+#         palette=palette,
+#         legend=i == 0,
+#     )
+#     axs[i].set_xscale("log", base=2)
+#     axs[i].set(
+#         title=f"$\lambda$ = {l}",
+#         xlabel="Patch size",
+#         ylabel="Value",
+#     )
+# handles, labels = axs[0].get_legend_handles_labels()
+# axs[0].legend(handles=handles, labels=labels, title="$\\beta$")
+# fig.suptitle("Value of state-aggregated representations")
+# save_figure(fig, "state_agg_value")
+
+# optimal patch size plot
+results_, lambdas = [], jnp.linspace(0, 20, 501)
+for l, beta, seed in tqdm(list(product(lambdas, betas, results["seed"].unique()))):
+    tmp = results[(results["beta"] == beta) & (results["seed"] == seed)]
+    tmp["value"] = tmp["accuracy"] - 0.5 - (l * tmp["cost"])
+    max_value, max_ps = (
+        tmp["value"].max(),
+        tmp["pool_size"].iloc[tmp["value"].argmax()],
+    )
+    results_.append(
+        {
+            "beta": beta,
+            "lambda": float(l),
+            "value": float(max_value),
+            "pool_size": int(max_ps),
+        }
+    )
+
+fig, ax = plt.subplots(figsize=(6, 4))
 sns.lineplot(
-    data=results,
-    x="pool_size",
-    y="accuracy",
+    data=pd.DataFrame(results_),
+    x="lambda",
+    y="pool_size",
     hue="beta",
-    style="type",
-    markers=True,
-    dashes=True,
     ax=ax,
-    palette=sns.color_palette("husl", n_colors=len(betas)),
-)
-ax.set_xscale("log", base=2)
-ax.set(
-    title="Predicted vs empirical accuracy using state-aggregated representations",
-    xlabel="Patch size",
-    ylabel="Choice prediction accuracy",
-    ylim=(0.45, 1.05),
+    palette=palette,
 )
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles=handles, labels=labels)
-plt.savefig("../figures/state_agg_experiment.pdf", bbox_inches="tight")
-
-# rng_key = random.PRNGKey(int(time.time()))
-# n_trials, beta = 10000, 0.1
-# keys = random.split(rng_key, n_trials)
-# u = sample_utility_function(rng_key, 128, lengthscale=2)
-# score = 0
-
-# for i in tqdm(range(n_trials)):
-#     pair = random.choice(rng_key, jnp.arange(len(u)), shape=(2,), replace=False)
-#     choice = choose(keys[i], pair[0], pair[1], u, beta)
-#     if u[pair[0]] > u[pair[1]]:
-#         score += int(choice == pair[0])
-#     else:
-#         score += int(choice == pair[1])
-
-# print(score / n_trials)
+ax.legend(handles=handles, labels=labels, title="$\\beta$")
+ax.set_yscale("log", base=2)
+ax.set(
+    title="Optimal patch size for state-aggregated representations",
+    xlabel="$\lambda$",
+    ylabel="Patch size",
+)
+save_figure(fig, "state_agg_optimal_patch_size")
--- python/src/modelling.py
+++ python/src/modelling.py
@@ -4,52 +4,49 @@ import GPy
 import jax.numpy as jnp
 from jax import random
 
-from .utils import to_range, boltzmann_1d
+from .utils import to_range
+
+
+def sample_covariance_matrix(
+    n_options: int,
+    variance=1,
+    lengthscale=1,
+) -> jnp.ndarray:
+    kernel = GPy.kern.RBF(1, variance=variance, lengthscale=lengthscale)
+    X = jnp.arange(n_options)[:, None]
+    return kernel.K(X, X)
 
 
 def sample_utility_function(
     rng_key: random.KeyArray,
-    n_options: int,
-    lengthscale=1,
+    cov: jnp.ndarray,
     min_val=-1,
     max_val=1,
 ) -> jnp.ndarray:
-    kernel = GPy.kern.RBF(1, variance=1, lengthscale=lengthscale)
-    X = jnp.arange(n_options)[:, None]
-    K = kernel.K(X, X)
-    u = random.multivariate_normal(rng_key, jnp.zeros(n_options), K)
+    n_options = cov.shape[0]
+    u = random.multivariate_normal(rng_key, jnp.zeros(n_options), cov)
     return to_range(u, min_val, max_val)
 
 
-def expected_delta(u: jnp.ndarray) -> float:
+def expected_acc_optimal(u: jnp.ndarray, beta: float) -> float:
     deltas = jnp.abs(u[:, None] - u[None, :])
-    return float(jnp.sum(deltas) / (len(u) * (len(u) - 1)))
+    tmp = 1 / (1 + jnp.exp(-deltas / beta))
+    return float(jnp.mean(tmp))
 
 
-def expected_delta_sign_product(u: jnp.ndarray, u_hat: jnp.ndarray) -> float:
+def expected_acc_approx(u: jnp.ndarray, u_hat: jnp.ndarray, beta: float) -> float:
     u_deltas = u[:, None] - u[None, :]
     u_hat_deltas = u_hat[:, None] - u_hat[None, :]
-    sign_product = jnp.sign(jnp.multiply(u_deltas, u_hat_deltas))
-    return float(jnp.sum(sign_product) / (len(u) * (len(u) - 1)))
-
-
-def expected_return_optimal(u: jnp.ndarray, beta: float) -> float:
-    e_delta = expected_delta(u)
-    return float(1 / (jnp.exp(-e_delta / beta) + 1))
-
-
-def expected_return_approx(u: jnp.ndarray, u_hat: jnp.ndarray, beta: float) -> float:
-    e_delta = expected_delta(u)
-    e_delta_sign_prod = expected_delta_sign_product(u, u_hat)
-    tmp = float(e_delta_sign_prod * jnp.tanh(e_delta / (2 * beta)))
-    return 0.5 + (tmp / 2)
+    sign_prod = jnp.sign(jnp.multiply(u_deltas, u_hat_deltas))
+    tmp = jnp.multiply(sign_prod, jnp.tanh(jnp.abs(u_deltas) / (2 * beta)))
+    return 0.5 + (float(jnp.mean(tmp)) / 2)
 
 
 def choose(
     rng_key: random.KeyArray, i: int, j: int, u: jnp.ndarray, beta: float
 ) -> int:
-    p = boltzmann_1d(jnp.array([u[i], u[j]]), beta)
-    return int(random.choice(rng_key, jnp.array([i, j]), p=p))
+    p_choose_i = 1 / (1 + jnp.exp((u[j] - u[i]) / beta))
+    return i if random.uniform(rng_key) < p_choose_i else j
 
 
 def simulate_choices(
@@ -78,10 +75,3 @@ def simulate_predictions(
     tmp = jnp.argmax(u_hat[pairs], axis=1)
     pred = pairs[jnp.arange(len(pairs)), tmp]
     return float(jnp.mean(pred == choices))
-    # score = 0
-    # for i in range(len(pairs)):
-    #     if u_hat[pairs[i, 0]] > u_hat[pairs[i, 1]]:
-    #         score += int(choices[i] == pairs[i, 0])
-    #     else:
-    #         score += int(choices[i] == pairs[i, 1])
-    # return score / len(pairs)
